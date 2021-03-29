9978e2d17a46a3f15435ccdcab003435961c898f
hai <punchcleric85@gmail.com>
2021-03-29 10:24:21 +0300

    project upd

--- delorian/src/main/scala/Boot.scala
+++ delorian/src/main/scala/Boot.scala
@@ -1,5 +1,32 @@
 package com.haile.app
 
-object Boot {
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import com.typesafe.scalalogging.Logger
 
+
+
+object Boot {
+  def main(args: Array[String]): Unit = {
+
+    implicit val system           = ActorSystem(Behaviors.empty, "my-system")
+    // are important for using flatMap onComplete at the end
+    implicit val executionContext = system.executionContext
+
+    val log = Logger("Boot")
+
+    val route =
+      path("date") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, java.time.LocalDate.now.toString))
+        }
+     }
+
+    val bindingFuture = Http().newServerAt("0.0.0.0", 8080).bind(route)
+
+    log.info("done")
+  }
 }
